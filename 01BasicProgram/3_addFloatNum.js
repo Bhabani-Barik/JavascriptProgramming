4108462aae03a6c5526c9694f5677be74da60861
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2024-04-19 14:07:54 +0530

    Approach 1 done

---
+++ 01BasicProgram/3_addFloatNum.js
@@ -0,0 +1,13 @@
+/** Javascript program to Add Float Numbers. 
+ - There are two methods to solve this problem, i.e-
+    1. Using parseFloat() and toFixed() method
+    2. Using parseFloat() and Math.round() method
+*/
+
+// Approach 1: Using parseFloat() and toFixed() method
+let val = parseFloat('2.3') + parseFloat('2.4');
+console.log("2.3 + 2.4 = " + val);
+function sumOfFloatNum() {
+    console.log("2.3 + 2.4 = " + (parseFloat('2.3') + parseFloat('2.4')).toFixed(2));
+}
+sumOfFloatNum();
