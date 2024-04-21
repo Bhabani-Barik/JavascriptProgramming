4bc519fd16a83b9ac6943d828defe28e8160801f
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2024-04-21 06:47:36 +0530

    Approach 1 and 2 done

---
+++ 01BasicProgram/3_oddOrEvenNum.js
@@ -0,0 +1,29 @@
+/**Javascript program to check if a number is Odd or Even. 
+    Even = {2k : k ∈ Z}
+    Odd = {2k + 1 : k ∈  Z}
+    where k is an integer.
+
+-  Check if a Number is Odd or Even, in several way i.e- 
+    1. Using the modulo operator
+    2. Using the Bitwise & operator
+    3. Using the Bitwise OR operator ( || )
+    4. using Ternary Operator
+*/
+// Approach 1: Using the modulo operator
+function isEven(n) {
+    return (n % 2 == 0);
+}
+
+let n = 101;
+ 
+isEven(n) ? console.log("Even") : console.log("Odd");
+
+// Approach 2: Using Bitwise & Operator
+function checkOddOrEven(n) {
+    if ( n & 1 == 1) {
+        return "Number is odd";
+    }
+    return "Number is Even";
+}
+console.log(checkOddOrEven(12)); //odd
+console.log(checkOddOrEven(1231)); //even
