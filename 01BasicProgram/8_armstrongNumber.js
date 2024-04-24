64d816de49d7db942f6a6489cc953a979285bbb0
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2024-04-24 06:08:47 +0530

    Approach 2 complete

--- 01BasicProgram/8_armstrongNumber.js
+++ 01BasicProgram/8_armstrongNumber.js
@@ -36,3 +36,41 @@ function isArmstrong(number){
 isArmstrong(9474);
 isArmstrong(520);
 
+// Approach 2: Using naive method
+
+function isArmstrong(number) {
+	let temp = number;
+	let o = order(temp)
+	let sum = 0;
+
+	// Loop until temp is greater than 0
+	while (temp) {
+		remainder = temp % 10;
+
+		// Floor value of the quotient
+		temp = Math.floor(temp / 10);
+		sum = sum + Math.pow(remainder, o);
+	}
+	if (sum === number) {
+		console.log(number + " is an Armstrong Number");
+	}
+	else {
+		console.log(number + " is Not an Armstrong Number");
+	}
+}
+
+// Function to calculate number of digits
+function order(number) {
+	let n = 0;
+	while (number > 0) {
+		n++;
+		number = Math.floor(number / 10);
+	}
+	return n;
+}
+
+// Input value 153
+isArmstrong(153);
+
+// Input value 520
+isArmstrong(520);
